dd400d255e1eb1058de665d83546cdd01a3c7a7e
viveksharma2022 <viveksharma.prabhakara@gmail.com>
2024-05-20 19:11:01 +0200

    player movement left right added

--- star_ship/star_ship/starShip.cpp
+++ star_ship/star_ship/starShip.cpp
@@ -11,11 +11,33 @@ void Game::PollEvents() {
         case sf::Event::Closed:
             mWindow.close();
             break;
+        case sf::Event::KeyPressed:
+            HandlePlayerInputs(event.key.code);
+            break;
             // we don't process other types of events
         default:
             break;
         }
     }
+}
+
+void Game::HandlePlayerInputs(sf::Keyboard::Key key) {
+    sf::Vector2f playerCurrentPosition = starShip->GetPlayerPosition();
+    if (key == sf::Keyboard::Left) {
+        starShip->SetPlayerPosition({playerCurrentPosition.x - 1.0f * MOV_RESOLUTION, playerCurrentPosition.y});
+    }
+    else if (key == sf::Keyboard::Right) {
+        starShip->SetPlayerPosition({ playerCurrentPosition.x + 1.0f * MOV_RESOLUTION, playerCurrentPosition.y });
+    }
+}
+
+void Player::SetPlayerPosition(sf::Vector2f newPosition) {
+    this->position = newPosition;
+    this->playerHost.setPosition(this->position);
+}
+
+const sf::Vector2f& Player::GetPlayerPosition() {
+    return this->position;
 }
 
 void Game:: Run() {
@@ -27,6 +49,7 @@ void Game:: Run() {
 
 void Render_API::Render(Game* currentGame) {
     currentGame->GetWindow().clear();
-    currentGame->GetWindow().draw(currentGame->GetPlayer()->GetPlayerPlace());
+    currentGame->GetWindow().draw(currentGame->GetPlayer()->GetPlayerHost());
     currentGame->GetWindow().display();
 }
+
--- star_ship/star_ship/starShip.h
+++ star_ship/star_ship/starShip.h
@@ -7,18 +7,35 @@
 
 #define WINDOW_WIDTH 800
 #define WINDOW_LENGTH 600
+#define PLAYER_WIDTH 40
+#define PLAYER_HEIGHT 50
+#define MOV_RESOLUTION PLAYER_WIDTH // movement resolution on the screen
 
 class Game;
+const std::string playerImage = "resources\\starShip.png";
 
 class Player {
 private:
-	sf::RectangleShape playerPlace;
-	sf::Vector2i position;
+	sf::RectangleShape playerHost; // host position of the player on the screen
+	sf::Vector2f position;
+	sf::Texture playerTexture;
 public:
-	Player():
-		position({ WINDOW_WIDTH / 2, WINDOW_LENGTH / 2 }),
-		playerPlace(sf::Vector2f(50.f, 120.f)) {}
-	sf::RectangleShape& GetPlayerPlace() { return playerPlace; }
+	Player() :
+		position({ WINDOW_WIDTH / 2, WINDOW_LENGTH - PLAYER_HEIGHT-1}),
+		playerHost(sf::Vector2f(PLAYER_WIDTH, PLAYER_HEIGHT))
+	{
+		playerHost.setPosition(position); // set the initial position of the player
+		if (playerTexture.loadFromFile(playerImage)){
+			playerHost.setTexture(&playerTexture);// set the texture for the player
+		} 
+	}
+	~Player() {
+		std::cout << "Player destructed" << std::endl;
+	}
+	// utlity functions for player
+	sf::RectangleShape&			GetPlayerHost() { return playerHost; }
+	void						SetPlayerPosition(sf::Vector2f newPosition);
+	const sf::Vector2f&			GetPlayerPosition();
 };
 
 class Render_API {	
@@ -39,8 +56,9 @@ public:
 		renderAPI(renderAPI),
 		starShip(std::make_unique<Player>())
 	{}
-	void PollEvents();
-	void Run();
-	sf::RenderWindow& GetWindow() { return this->mWindow; }
-	std::unique_ptr<Player>& GetPlayer() { return this->starShip; }
+	void						PollEvents();
+	void						Run();
+	sf::RenderWindow&			GetWindow() { return this->mWindow; }
+	std::unique_ptr<Player>&	GetPlayer() { return this->starShip; }
+	void						HandlePlayerInputs(sf::Keyboard::Key key);
 };
